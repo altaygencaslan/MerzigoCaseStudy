b0c68679ddc61889598995f5eb24b70047815dce
Altay GENÇASLAN <developer.gencaslan@gmail.com>
2024-10-12 03:41:24 +0300

    feat: Created all entities and created admin user when migration run

---
+++ ContentAPI.Business/ContentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContentAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentAPI.Business
+{
+    public class ContentDbContext : DbContext
+    {
+        public DbSet<Content> Contents { get; set; }
+    }
+}
---
+++ UserAPI.Business/UserDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserAPI.Domain;
+
+namespace UserAPI.Business
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "admin",
+                    LastName = "admin",
+                    CreatedDate = DateTime.Now,
+                    CreatedUserId = Guid.Empty,
+                    UpdatedDate = DateTime.Now,
+                    UpdatedUserId = Guid.Empty,
+                    IsDeletable = false,
+                    IsDeleted = false,
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
